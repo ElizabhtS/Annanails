fe9e384e16927cfbc69b5e41f2a8e14b8c32accb
ElizabhtS <96659852+ElizabhtS@users.noreply.github.com>
2022-06-25 22:17:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let imgItem = document.querySelector('.imgs');
+imgItem.onclick = function(){
+    let mySrc = imgItem.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        imgItem.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        imgItem.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+
+let imgIt = document.querySelector('.im');
+imgIt.onclick = function(){
+    let mySrc = imgIt.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        imgIt.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        imgIt.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+let imgIte = document.querySelector('.img');
+imgIte.onclick = function(){
+    let mySrc = imgIte.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        imgIte.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        imgIte.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+let oimgItem = document.querySelector('.oimgs');
+oimgItem.onclick = function(){
+    let mySrc = oimgItem.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        oimgItem.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        oimgItem.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+let oimgIte = document.querySelector('.oimg');
+oimgIte.onclick = function(){
+    let mySrc = oimgIte.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        oimgIte.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        oimgIte.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+let oimgIt = document.querySelector('.oim');
+oimgIt.onclick = function(){
+    let mySrc = oimgIt.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        oimgIt.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        oimgIt.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+let oimgI = document.querySelector('.oi');
+oimgI.onclick = function(){
+    let mySrc = oimgI.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        oimgI.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        oimgI.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
+let imgI = document.querySelector('.i');
+imgI.onclick = function(){
+    let mySrc = imgI.getAttribute('src');
+    if(mySrc === '../img/like_favorite_heart_3524 (1).png'){
+        imgI.setAttribute ('src','../img/heart_like_love_twitter_icon_127132.png')
+    }else{
+        imgI.setAttribute ('src','../img/like_favorite_heart_3524 (1).png');
+    }
+}
